f45dbb4894a9d4e0c868bbc0c9e421c34b130d91
Oskar <oskar@MacBook-Pro-Marcin.local>
2022-10-30 13:48:34 +0100

    commite

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ $("#contactMe").on("click", () => {
   var link = "mailto:oskarrochowiak@gmail.com";
   window.location.href = link;
 });
+
+$("li").on("click", (e) => {
+  const idClicked = e.target.id;
+  $("#" + idClicked).addClass("chosen");
+});
