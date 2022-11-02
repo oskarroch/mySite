d3dd7e2e2b765f99a8d21bf2129e9197c5402385
Oskarrro03 <oskarrochowiak@gmail.com>
2022-11-02 21:52:32 +0100

    commit

--- index.js
+++ index.js
@@ -1,36 +1,19 @@
-$("document").ready(() => {
-  $("#abtMeDot").addClass("chosen");
-});
-
 $("#contactMe").on("click", () => {
   var link = "mailto:oskarrochowiak@gmail.com";
   window.location.href = link;
 });
 
-$("#abtMe").on("click", () => {
-  $("#circle").animate({ top: "51px" }, 1000);
-});
-
-$("#featuredWork").on("click", () => {
-  $("#circle").animate({ top: "134px" }, 1000);
-});
-
-$("#xp").on("click", () => {
-  $("#circle").animate({ top: "217px" }, 1000);
-});
-
-$("#interests").on("click", () => {
-  $("#circle").animate({ top: "300px" }, 1000);
-});
-
-function myFunction(x) {
-  if (x.matches) {
-    $("#nav").css({ width: "1px" });
-  } else {
-    $("#nav").css({ width: "170px" });
+$(window).on("load resize", function () {
+  if ($(window).width() < 750) {
+    $(".abtMe, .featuredWork, .xp, .interests").text("");
+    $(".dot").css({ left: "10px" });
+    $("#circle").css({ right: "14px" });
+  } else if ($(window).width() > 750) {
+    $(".abtMe").text("About Me");
+    $(".featuredWork").text("Featured Work");
+    $(".xp").text("Experience");
+    $(".interests").text("Interests");
+    $(".dot").css({ left: "180px" });
+    $("#circle").css({ right: "13px" });
   }
-}
-
-var x = window.matchMedia("(max-width: 750px)");
-myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
+});
